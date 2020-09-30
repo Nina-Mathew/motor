ef7e51e88b2856e36fa0a05826ab221a32f5f03a
Nina-Mathew <71400436+Nina-Mathew@users.noreply.github.com>
2020-09-30 13:09:49 -0400

    made motor turn

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+// Created by: Nina Mathew
+// Created on: 9/30/20
+// This program makes the motor turn
+robotbit.Servo(robotbit.Servos.S1, 0)
+basic.pause(1000)
+robotbit.Servo(robotbit.Servos.S1, 180)
+basic.pause(1000)
+robotbit.Servo(robotbit.Servos.S1, 0)
+basic.pause(1000)
+robotbit.Servo(robotbit.Servos.S1, 180)
+basic.pause(1000)
+robotbit.Servo(robotbit.Servos.S1, 0)
 basic.forever(function () {
 	
 })
